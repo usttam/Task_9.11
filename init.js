a34e4190624ff092591b923d24e48be70dc8f7ee
usttam <tamulinas.ustinas@hotmail.com>
2022-12-24 13:39:11 +0300

    add familyName Json data for male and female, add occupation Json data, add fun randomFamilyName for generate random Family Name, update randomBirth func for output month in letters,add randomOccupation func for generate random job proffesion

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
 
 window.onload = function()
 {
-    const initPerson = personGenerator.getPerson();
+    const initPerson = personGenerator.getPerson();    
     document.querySelector('#firstNameOutput').innerText = initPerson.firstName;
     document.querySelector('#surnameOutput').innerText = initPerson.lastName;
     document.querySelector('#genderOutput').innerText = initPerson.gender;
     document.querySelector('#birthYearOutput').innerText = initPerson.birthDate;
+    document.querySelector('#familyNameOutput').innerText = initPerson.familyName;
+    document.querySelector('#professionOutput').innerText = initPerson.jobProffesion;
 };
 
--- personGenerator.js
+++ personGenerator.js
@@ -35,7 +35,7 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
-    firstNameFeMaleJson: `{
+    firstNameFemaleJson: `{
         "count": 10,
         "list": {     
             "id_1": "Елена",
@@ -48,6 +48,57 @@ const personGenerator = {
             "id_8": "Мария",
             "id_9": "Валерия",
             "id_10": "Дарья"
+        }
+    }`,
+    femaleFamilyNameJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Олеговна",
+            "id_2": "Павловна",
+            "id_3": "Юрьевна",
+            "id_4": "Яковлевна",
+            "id_5": "Степановна",
+            "id_6": "Львовна",
+            "id_7": "Олеговна",
+            "id_8": "Богдановна",
+            "id_9": "Борисовна",
+            "id_10": "Георгиевна"
+        }
+    }`,
+    maleFamilyNameJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Иванович",
+            "id_2": "Смирнович",
+            "id_3": "Кузнецович",
+            "id_4": "Попович",
+            "id_5": "Васильевич",
+            "id_6": "Петрович",
+            "id_7": "Соколович",
+            "id_8": "Михайлович",
+            "id_9": "Медведевич",
+            "id_10": "Орехович"
+        }
+    }`,
+    occupationJson: `{
+        "count": 15,
+        "list": {
+            "id_1": "Водитель",
+            "id_2": "Электромонтер",
+            "id_3": "Программист",
+            "id_4": "Врач",
+            "id_5": "Менеджер",
+            "id_6": "Официант",
+            "id_7": "Биотехнолог",
+            "id_8": "Дизайнер",
+            "id_9": "Воспитатель",
+            "id_10": "Секретарь",
+            "id_11": "Косметолог",
+            "id_12": "Швея",
+            "id_13": "Оператор",
+            "id_14": "Медсестра",
+            "id_15": "Слесарь",
+            "id_16": "Инженер"
         }
     }`,
 
@@ -66,7 +117,7 @@ const personGenerator = {
         if (gender===this.GENDER_MALE){
             return this.randomValue(this.firstNameMaleJson);   
         } else  {
-            return this.randomValue(this.firstNameFeMaleJson);  
+            return this.randomValue(this.firstNameFemaleJson);  
         } 
     },
 
@@ -88,18 +139,87 @@ const personGenerator = {
     },  
     
     randomBirthDate: function() {
-        return new Date(this.randomIntNumber(2010,1950),this.randomIntNumber(12),this.randomIntNumber(31)).toLocaleDateString();
-1       /*если функция Date вызывается в качестве конструктора с более, 
+        const randomDate = new Date(this.randomIntNumber(2010,1950),this.randomIntNumber(12),this.randomIntNumber(31));
+        /*если функция Date вызывается в качестве конструктора с более, 
          чем одним аргументом, значения, большие логического диапазона 
          (например, 13 в качестве номера месяца) «переметнутся» на соседние значения*/
-0    },    
+        let monthText='';
+        switch (randomDate.getMonth()) {
+            case 0:
+                monthText = 'Января'; 
+                break;
+            case 1:   
+                monthText = 'Февраля';
+                break;
+            case 2:   
+                monthText = 'Марта';
+                break;                
+            case 3: 
+                monthText = 'Аперля';
+                break;               
+            case 4:  
+                monthText = 'Мая';
+                break;                   
+            case 5:   
+                monthText = 'Июня';
+                break;                
+            case 6:  
+                monthText = 'Июля';
+                break;                
+            case 7:   
+                monthText = 'Августа';
+                break;                
+            case 8:  
+                monthText = 'Сентября';
+                break;                
+            case 9:   
+                monthText = 'Октября';
+                break;                 
+            case 10:   
+                monthText = 'Ноября';
+                break;                 
+            case 11: 
+                monthText = 'Декабря';
+                break;                   
+        }
+        return `${randomDate.getDate()} ${monthText} ${randomDate.getFullYear()}г.`;
+    },
+
+    randomFamilyName: function(gender) {
+        if (gender===this.GENDER_MALE){
+            return this.randomValue(this.maleFamilyNameJson);   
+        } else  {
+            return this.randomValue(this.femaleFamilyNameJson);  
+        } 
+    },
+
+    randomOccupation: function(gender) {
+        let jobTitle='';        
+            do {
+                jobTitle=this.randomValue(this.occupationJson);
+                //debugger;
+                if (gender===this.GENDER_MALE){
+                    if (jobTitle!=="Косметолог" && jobTitle!=="Воспитатель" && jobTitle!=="Швея"&& jobTitle!=="Медсестра"){
+                        break;
+                    }                    
+                } else {
+                    if (jobTitle!=="Слесарь" && jobTitle!=="Оператор" && jobTitle!=="Электромонтер" && jobTitle!=="Водитель" & jobTitle!=="Инженер"){
+                        break;
+                    }           
+                }
+            } while (true);           
+            return jobTitle; 
+       
+    },
 
     getPerson: function () {
-        this.person = {};       
+        this.person = {};                   
         this.person.gender=this.randomGender();        
         this.person.firstName = this.randomFirstName(this.person.gender);
         this.person.lastName = this.randomSurname(this.person.gender);
         this.person.birthDate = this.randomBirthDate();
+        this.person.familyName= this.randomFamilyName(this.person.gender);
+        this.person.jobProffesion= this.randomOccupation(this.person.gender);
         return this.person;
     }
 };
