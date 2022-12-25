7316b2b95bb5fd8d5bb5eb10188eacdaaafb661f
usttam <tamulinas.ustinas@hotmail.com>
2022-12-25 15:49:37 +0300

    add style.css, readme.md info

--- init.js
+++ init.js
@@ -10,3 +10,29 @@ window.onload = function()
     document.querySelector('#professionOutput').innerText = initPerson.jobProffesion;
 };
 
+document.querySelector('#btnGenerator').addEventListener('click',()=>{
+    const initPerson = personGenerator.getPerson();    
+    document.querySelector('#firstNameOutput').innerText = initPerson.firstName;
+    document.querySelector('#surnameOutput').innerText = initPerson.lastName;
+    document.querySelector('#genderOutput').innerText = initPerson.gender;
+    document.querySelector('#birthYearOutput').innerText = initPerson.birthDate;
+    document.querySelector('#familyNameOutput').innerText = initPerson.familyName;
+    document.querySelector('#professionOutput').innerText = initPerson.jobProffesion;
+})
+
+document.querySelector('#btnClear').addEventListener('click',()=>{
+    document.querySelector('#firstNameOutput').innerText = '[Имя]';
+    document.querySelector('#surnameOutput').innerText = '[Фамилия]';
+    document.querySelector('#genderOutput').innerText = '[Пол]';
+    document.querySelector('#birthYearOutput').innerText = '[Дата рождения]';
+    document.querySelector('#familyNameOutput').innerText = '[Отчество]';
+    document.querySelector('#professionOutput').innerText = '[Профессия]';
+
+    /*document.querySelector('#firstNameOutput').innerText = '';
+    document.querySelector('#surnameOutput').innerText = '';
+    document.querySelector('#genderOutput').innerText = '';
+    document.querySelector('#birthYearOutput').innerText = '';
+    document.querySelector('#familyNameOutput').innerText = '';
+    document.querySelector('#professionOutput').innerText = '';*/
+
+})
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Generator of random user data: full name, gender, date of birth and profession..
+
+Developed as a practical task on the course '_Full-stack developer'_
+---
+
+## Technologies used:
+
+* HTML
+
+* CSS
+
+* Visual Studio Code
+
+* JavaScript
+
+* Bootstrap
+
+## How to open/run
+
+Go to the site folder in the file manager, double-click on the ___index.html___ file.
+
+
